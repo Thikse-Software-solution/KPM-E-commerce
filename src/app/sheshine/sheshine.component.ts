b89b5b215906e30592f4a756481fc1c5ee106be2
SaravananSundar <saravananmech020797@gmail.com>
2024-07-30 17:05:17 +0530

    Design the page

--- src/app/sheshine/sheshine.component.ts
+++ src/app/sheshine/sheshine.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sheshine',
   templateUrl: './sheshine.component.html',
   styleUrl: './sheshine.component.scss'
 })
-export class SheshineComponent {
-  menuActive = false;
 
-  toggleMenu() {
-    this.menuActive = !this.menuActive;
-    console.log('Menu active:', this.menuActive); // Debug log to check toggle functionality
+export class SheshineComponent {
+  constructor(private router: Router) {}
+
+  closeNavbar() {
+    const navbarCollapse = document.getElementById('navbarNav');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+    }
   }
 }
+// export class SheshineComponent {
+//   menuActive = false;
+
+//   toggleMenu() {
+//     this.menuActive = !this.menuActive;
+//     console.log('Menu active:', this.menuActive); // Debug log to check toggle functionality
+//   }
+// }
